2fe83ff75ec9b48c08ab572f8bab4e1632759e72
codingblazer <sasachin04@gmail.com>
2017-02-16 03:03:14 +0530

    Added Themes and styles

--- app/src/main/java/com/example/sachinaggarwal/tnm17/SplashActivity.java
+++ app/src/main/java/com/example/sachinaggarwal/tnm17/SplashActivity.java
@@ -3,11 +3,18 @@ package com.example.sachinaggarwal.tnm17;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
+
+
+
     }
 }
