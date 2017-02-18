45e3f887844bd863ebb0af67b34c810a38ce8f29
codingblazer <sasachin04@gmail.com>
2017-02-19 05:03:21 +0530

    Completed Home Fragment- Added Moving Sliders

--- app/src/main/java/com/example/sachinaggarwal/tnm17/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/sachinaggarwal/tnm17/Fragments/HomeFragment.java
@@ -5,14 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.example.sachinaggarwal.tnm17.R;
 
+import java.util.HashMap;
+
 /**
- * Created by HIman$hu on 2/19/2017.
+ * Created by codingblazer on 2/19/2017.
  */
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
+    private SliderLayout mDemoSlider;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +33,63 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        mDemoSlider = (SliderLayout) view.findViewById(R.id.slider);
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("AboutUs", R.drawable.og2);
+        file_maps.put("ChiefGuests",R.drawable.download);
+        file_maps.put("Gallery",R.drawable.gal);
+        file_maps.put("Intro",R.drawable.logofull);
+
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.FlipPage);
+        //or use flip horizontal, fade, tablet,rotate down, Zoom out slide were looking good to me
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(2000);
+        mDemoSlider.startAutoCycle();
+
+
+
         return view;
+    }
+    @Override
+    public void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
