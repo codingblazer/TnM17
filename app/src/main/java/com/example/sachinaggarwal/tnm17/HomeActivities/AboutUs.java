747647bd41ffe0fd7b9bb6a438dd3c32b2f53f91
HimanshuS01 <himanshu.singhal116@gmail.com>
2017-03-09 01:59:11 +0530

    Different screens for home fragment added

---
+++ app/src/main/java/com/example/sachinaggarwal/tnm17/HomeActivities/AboutUs.java
@@ -0,0 +1,15 @@
+package com.example.sachinaggarwal.tnm17.HomeActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sachinaggarwal.tnm17.R;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_screen_in_home);
+    }
+}
--- app/src/main/java/com/example/sachinaggarwal/tnm17/ParallaxViewPager/SampleListFragment.java
+++ app/src/main/java/com/example/sachinaggarwal/tnm17/ParallaxViewPager/SampleListFragment.java
@@ -1,7 +1,6 @@
 package com.example.sachinaggarwal.tnm17.ParallaxViewPager;
 
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -14,7 +13,6 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,7 +20,6 @@ import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
 import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
-import com.example.sachinaggarwal.tnm17.EventDetails;
 import com.example.sachinaggarwal.tnm17.Fragments.Schedule;
 import com.example.sachinaggarwal.tnm17.R;
 
@@ -33,27 +30,14 @@ public class SampleListFragment extends ScrollTabHolderFragment {
     private static final String ARG_POSITION = "position";
 
 
-    //<<<<<<< HEAD
     private ListView mListView;
-    //    private SwipeMenuListView mList;
-//        private ArrayList<String> mListItems;
-//
-//    private int mPosition;
-//
-//    String[] events_1;
-//    int[] images;
-//    String[] timing;
-//    String[] intents;
-//    TextView eventName, act_name;
-//
-//=======
-    //    private ListView mListView;
+
+    //private ListView mListView;
     private SwipeMenuListView mList;
     private ArrayList<String> mListItems;
-    private ArrayList<String> mListCode;
+
     private int mPosition;
 
-    //>>>>>>> parallax package
 
     public static Fragment newInstance(int position) {
         SampleListFragment f = new SampleListFragment();
@@ -70,62 +54,11 @@ public class SampleListFragment extends ScrollTabHolderFragment {
 
 
         mListItems = new ArrayList<String>();
-        mListCode = new ArrayList<String>();
 
-if(mPosition==0) {
-    mListItems.add("Choreo Elims");
-    mListItems.add("Street Dance");
-    mListItems.add("Desert Duel");
-    mListItems.add("Razzmatazz Elims");
-    mListItems.add("Rock Dance");
-    mListItems.add("Funk in Motion");
-    mListItems.add("bhangra Dance");
+        for (int i = 1; i <= 100; i++) {
+            mListItems.add(i + ". item - currnet page: " + (mPosition + 1));
+        }
 
-    mListCode.add("CL2");
-    mListCode.add("TS4");
-    mListCode.add("NT5");
-    mListCode.add("SP2");
-    mListCode.add("CL2");
-    mListCode.add("TS3");
-    mListCode.add("SP6");
-
-
-}
-        else if(mPosition==1){
-    mListItems.add("Choreo Elims");
-    mListItems.add("Street Dance");
-    mListItems.add("Desert Duel");
-    mListItems.add("Razzmatazz Elims");
-    mListItems.add("Rock Dance");
-    mListItems.add("Funk in Motion");
-    mListItems.add("bhangra Dance");
-
-    mListCode.add("CL2");
-    mListCode.add("TS4");
-    mListCode.add("NT5");
-    mListCode.add("SP2");
-    mListCode.add("CL2");
-    mListCode.add("TS3");
-    mListCode.add("SP6");
-}
-
-else if(mPosition==2){
-    mListItems.add("Choreo Elims");
-    mListItems.add("Street Dance");
-    mListItems.add("Desert Duel");
-    mListItems.add("Razzmatazz Elims");
-    mListItems.add("Rock Dance");
-    mListItems.add("Funk in Motion");
-    mListItems.add("bhangra Dance");
-
-    mListCode.add("CL2");
-    mListCode.add("TS4");
-    mListCode.add("NT5");
-    mListCode.add("SP2");
-    mListCode.add("CL2");
-    mListCode.add("TS3");
-    mListCode.add("SP6");
-}
     }
 
     @Override
@@ -190,51 +123,7 @@ else if(mPosition==2){
         mListView.setOnScrollListener(new OnScroll());
         mListView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.list_item, android.R.id.text1, mListItems));
 
-
-        if(mPosition==0){
-
-            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                    Intent intent1;
-                    intent1=new Intent(getContext(),EventDetails.class);
-
-                    intent1.putExtra("eventcode",mListCode.get(position-1));
-                    intent1.putExtra("eventName",mListItems.get(position-1));
-                    startActivity(intent1);
-                }
-            });
-        }
-
-        else if(mPosition==1){
-            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                    Intent intent1;
-                    intent1=new Intent(getContext(),EventDetails.class);
-                    intent1.putExtra("eventcode",mListCode.get(position-1));
-                    intent1.putExtra("eventName",mListItems.get(position-1));
-                    startActivity(intent1);
-                }
-            });
-        }
-
-        else if(mPosition==2){
-            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                    Intent intent1;
-                    intent1=new Intent(getContext(),EventDetails.class);
-                    intent1.putExtra("eventcode",mListCode.get(position-1));
-                    intent1.putExtra("eventName",mListItems.get(position-1));
-                    startActivity(intent1);
-                }
-            });
-        }
-        if(Schedule.NEEDS_PROXY){//in my moto phone(android 2.1),setOnScrollListener do not work well
+        if (Schedule.NEEDS_PROXY) {//in my moto phone(android 2.1),setOnScrollListener do not work well
             mListView.setOnTouchListener(new OnTouchListener() {
 
                 @Override
@@ -257,7 +146,7 @@ else if(mPosition==2){
 
     }
 
-    public class OnScroll implements OnScrollListener{
+    public class OnScroll implements OnScrollListener {
 
 
         @Override
@@ -281,184 +170,8 @@ else if(mPosition==2){
     }
 
 
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        View v = inflater.inflate(R.layout.schedule_list, null);
-//
-//        mList = (SwipeMenuListView) v.findViewById(R.id.schedule_categories_listView);
-//
-//        // step 1. create a MenuCreator
-//        SwipeMenuCreator creator = new SwipeMenuCreator() {
-//
-//            @Override
-//            public void create(SwipeMenu menu) {
-//                // create "open" item
-//                SwipeMenuItem openItem = new SwipeMenuItem(getContext());
-//                // set item background
-//                openItem.setBackground(new ColorDrawable(Color.rgb(0xf4, 0x43, 0x36)));
-//                // set item width
-//                openItem.setWidth(dp2px(90));
-//                // set item title
-//                openItem.setTitle("Open");
-//                // set item title fontsize
-//                openItem.setTitleSize(18);
-//                // set item title font color
-//                openItem.setTitleColor(Color.WHITE);
-//                // add to menu
-//                menu.addMenuItem(openItem);
-//
-//                // create "delete" item
-//                SwipeMenuItem deleteItem = new SwipeMenuItem(getContext());
-//                // set item background
-//                deleteItem.setBackground(new ColorDrawable(Color.rgb(0x28,
-//                        0x28, 0x28)));
-//                // set item width
-//                deleteItem.setWidth(dp2px(90));
-//                // set a icon
-//                deleteItem.setIcon(R.drawable.ic_settings);
-//                // add to menu
-//                menu.addMenuItem(deleteItem);
-//            }
-//        };
-//
-////        View placeHolderView = inflater.inflate(R.layout.view_header_placeholder, mListView, false);
-////        placeHolderView.setBackgroundColor(0xFFFFFFFF);
-////        mListView.addHeaderView(placeHolderView);
-//
-//        // set creator
-//        mList.setMenuCreator(creator);
-//        mList.setOpenInterpolator(new BounceInterpolator());
-//        mList.setCloseInterpolator(new BounceInterpolator());
-//
-//
-//        //Add events in this array
-//        events_1 = new String[]{"Alkhwarizm", "Blind War", "C Fresh", "C Hunt", "C Master", "Ex-Machina", "Humble Fool Cup", "Infinitum", "Three Muskeeters"};
-//
-//        images = new int[]{R.drawable.alkhwarizm, R.drawable.blindwar, R.drawable.cfresh, R.drawable.chunt, R.drawable.exmachina, R.drawable.humblefool};
-//
-//        timing = new String[]{"6:00 PM, CC3", "6:00 PM, CC3", "2:00 PM - 6:00 PM, CC3", "6:00 PM, CC3", "6:00 PM, CC3", "6:00 PM, CC3", "6:00 PM, CC3", "6:00 PM, CC3", "6:00 PM, CC3"};
-//
-//        intents = new String[]{"ALK", "BLI", "CFR", "CHU", "CMA", "EXM", "HUM", "INF", "THR"};
-//
-//        initList(events_1, images, timing, intents);
-//
-//        return v;
-//    }
-//
-//    public void initList(String[] eventsArray, int[] imagesList, String[] timingList, String[] intentsList) {
-//        if (eventsArray.length != 0) {
-//
-//            ArrayList<HashMap<String, String>> eventList = new ArrayList<HashMap<String, String>>();
-//
-//            for (int i = 0; i < eventsArray.length; i++) {
-//                HashMap<String, String> candy = new HashMap<String, String>();
-//                candy.put("event", eventsArray[i]);
-//                candy.put("image", Integer.toString(imagesList[i]));
-//                candy.put("time", timingList[i]);
-//                candy.put("intent", "com.app.aparoksha.apro16." + intentsList[i].trim());
-//                eventList.add(candy);
-//            }
-//
-//            ListAdapter adapter = new SimpleAdapter(
-//                    getContext(),
-//                    eventList,
-//                    R.layout.schedule_list_item,
-//                    new String[]{"event", "image", "time", "intent"},
-//                    new int[]{R.id.event_name, R.id.eventImg, R.id.eventTime, R.id.intent}) {
-//                @Override
-//                public View getView(int position, View convertView, ViewGroup parent) {
-//                    View view = super.getView(position, convertView, parent);
-//                    TextView item_name = (TextView) view.findViewById(R.id.event_name);
-//                    return view;
-//                }
-//            };
-//
-//            mList.setAdapter(adapter);
-//        }
-//
-//        mList.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
-//            @Override
-//            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
-//                switch (index) {
-//                    case 0:
-//                        // open
-//
-//                        String intentToOpen = "com.app.aparoksha.apro16." + intents[position];
-//
-//                        Intent i = new Intent(intentToOpen);
-//                        i.putExtra("INTENT", intentToOpen);
-//                        startActivity(i);
-//
-//                        break;
-//                    case 1:
-//                        Intent it = new Intent("com.app.aparoksha.apro16.SN");
-//                        it.putExtra("event", events_1[position]);
-//                        it.putExtra("address", "com.app.aparoksha.apro16." + intents[position]);
-//                        startActivity(it);
-//                        break;
-//                }
-//                return false;
-//            }
-//        });
-//
-//    }
-//
-
     private int dp2px(int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
                 getResources().getDisplayMetrics());
     }
-//
-//    @Override
-//    public void onActivityCreated(Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//
-////        mListView.setOnScrollListener(new OnScroll());
-////        mListView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.list_item, android.R.id.text1, mListItems));
-////
-////        if (Schedule.NEEDS_PROXY) {//in my moto phone(android 2.1),setOnScrollListener do not work well
-////            mListView.setOnTouchListener(new OnTouchListener() {
-////                @Override
-////                public boolean onTouch(View v, MotionEvent event) {
-////                    if (mScrollTabHolder != null)
-////                        mScrollTabHolder.onScroll(mListView, 0, 0, 0, mPosition);
-////                    return false;
-////                }
-////            });
-////        }
-//    }
-//
-//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-//    @Override
-//    public void adjustScroll(int scrollHeight) {
-//        if (scrollHeight == 0 && mList.getFirstVisiblePosition() >= 1) {
-//            return;
-//        }
-//
-//        mList.setSelectionFromTop(1, scrollHeight);
-//
-//    }
-//
-//    public class OnScroll implements OnScrollListener {
-//
-//        @Override
-//        public void onScrollStateChanged(AbsListView view, int scrollState) {
-//
-//        }
-//
-//        @Override
-//        public void onScroll(AbsListView view, int firstVisibleItem,
-//                             int visibleItemCount, int totalItemCount) {
-//            if (mScrollTabHolder != null)
-//                mScrollTabHolder.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount, mPosition);
-//        }
-//
-//    }
-//
-//
-//    @Override
-//    public void onScroll(AbsListView view, int firstVisibleItem,
-//                         int visibleItemCount, int totalItemCount, int pagePosition) {
-//    }
-
-
 }
