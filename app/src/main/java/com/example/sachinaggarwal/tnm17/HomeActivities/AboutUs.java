33f2894348cd6162aa605ccd686105dc018f2408
HimanshuS01 <himanshu.singhal116@gmail.com>
2017-04-06 19:41:19 +0530

    Merge conflict removed from manifest file

--- app/src/main/java/com/example/sachinaggarwal/tnm17/HomeActivities/AboutUs.java
+++ app/src/main/java/com/example/sachinaggarwal/tnm17/HomeActivities/AboutUs.java
@@ -10,6 +10,6 @@ public class AboutUs extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_us);
+//        setContentView(R.layout.activity_about_us);
     }
 }
